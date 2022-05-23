15b8386d9954c9a043f3c16e6c04c0ec01af5891
Tauhidul Khandaker <teijaskh@gmail.com>
2022-05-23 13:56:58 +0600

    Hello

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAk0q3sPuWntygVNiEloBoPFf5W9iGzcm0",
+    authDomain: "simple-ema-john-9e779.firebaseapp.com",
+    projectId: "simple-ema-john-9e779",
+    storageBucket: "simple-ema-john-9e779.appspot.com",
+    messagingSenderId: "740319350778",
+    appId: "1:740319350778:web:182ade0ac264544ce55e43"
+};
+
+export default firebaseConfig;
